13aaa286499ea12ce4bc8d2bd7bbb382baa0c515
Kevin Berendsen <10073027+kevto@users.noreply.github.com>
2022-05-25 14:27:04 +0200

    Improved cleanup and changed commit hash

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 kubectl delete pods,persistentvolumeclaims,pipelineruns,pipelines --all -n openvalue-ops
 kubectl delete deployments,replicasets,pods,services --all -n openvalue-prd
+gcloud artifacts docker images delete europe-west1-docker.pkg.dev/openvalue-tekton-cicd-talk/docker/hello-world-service
